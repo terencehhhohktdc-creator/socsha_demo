2b88dae301be3a643bc27ddd9668d3feee823c30
terencehhhohktdc-creator <terence.hh.ho.hktdc@gmail.com>
2026-07-02 16:24:12 +0800

    Update images.js

--- js/images.js
+++ js/images.js
@@ -1,5 +1,5 @@
 // images.js - Image paths configuration
 const images = {
   background: "./img/Gemini_Generated_Image_q2jrsoq2jrsoq2jr.png",
-  eventPhoto: "img/event-photo.jpg"
+  eventPhoto: "img/event-photo.png"
 };
